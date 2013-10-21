2630b8e8b4ee78a6cffe8cd9b6f62eab986ddeeb
Travis Petticrew <travis@petticrew.net>
2013-10-20 21:31:04 -0500

    Generated Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poll::Application.config.secret_key_base = 'ee0ff5c0717c0035c827e637ce4381d162883d9ac2c8958fdda0c59b209ea9f992b349d2412a6a8437972659b5a5a1518abfb73391180f1b07ba17054cdc724c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poll::Application.config.session_store :cookie_store, key: '_poll_session'
